26e5534c93d806ad5b4321799b4e6111749ad3e6
David J. Gardner <david.gardner@alere.com>
2020-12-04 13:56:41 -0500

    added 4 challenge, part 1

---
+++ AdventOfCode2020/Day4/Passport.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Day4
+{
+    public class Passport : List<KeyValuePair<string, string>>
+    {
+        public bool IsValid()
+        {
+            //example of valid
+            //ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+            //byr:1937 iyr:2017 cid:147 hgt:183cm
+            bool isValid = true;
+
+            var allKeys = this.Select(k => k.Key).ToArray();
+
+            isValid &= allKeys.Any(k => k == "ecl");
+            isValid &= allKeys.Any(k => k == "pid");
+            isValid &= allKeys.Any(k => k == "eyr");
+            isValid &= allKeys.Any(k => k == "hcl");
+            isValid &= allKeys.Any(k => k == "byr");
+            isValid &= allKeys.Any(k => k == "iyr");
+            isValid &= allKeys.Any(k => k == "hgt");
+            
+            return isValid;
+        }
+    }
+}
---
+++ AdventOfCode2020/Day4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var passports = new List<Passport>();
+
+            using (var reader = new StreamReader("input.txt"))
+            {
+                var passport = new Passport();
+
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    if (string.IsNullOrEmpty(line))
+                    {
+                        if (passport.Any())
+                        {
+                            passports.Add(passport);    
+                        }
+                        
+                        passport = new Passport();
+                        continue;
+                    }
+
+                    var entries = line.Split(' ');
+
+                    foreach (var entry in entries)
+                    {
+                        var pair = entry.Split(':');
+                        var key = pair[0];
+                        var val = pair[1];
+                        passport.Add(new KeyValuePair<string, string>(key, val));
+                    }
+                }
+            }
+
+            var totalPassports = passports.Count;
+            var validCount = passports.Count(p => p.IsValid());
+            
+            Console.WriteLine($"Total passports {totalPassports}, {validCount} are valid.");
+        }
+    }
+}
