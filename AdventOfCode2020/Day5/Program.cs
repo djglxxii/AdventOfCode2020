7f7c5c6950e03ddd7c68cf3b0e13eae73db46563
David J. Gardner <djglxxii@outlook.com>
2020-12-05 12:38:45 -0500

    day 5 solution, part 2

--- AdventOfCode2020/Day5/Program.cs
+++ AdventOfCode2020/Day5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
@@ -10,6 +11,10 @@ namespace Day5
         static void Main(string[] args)
         {
             Part1Solution();
+            Part2Solution();
+            
+            Console.WriteLine("Done! Press any key to exit.");
+            Console.ReadLine();
         }
 
         private static void Part1Solution()
@@ -28,8 +33,31 @@ namespace Day5
 
             var highestSeat = seats.Max(s => s.GetSeatNo());
 
-            Console.WriteLine(highestSeat);
-            Console.ReadLine();
+            Console.WriteLine($"Highest seat ID: {highestSeat}");
+        }
+
+        private static void Part2Solution()
+        {
+            var seats = new List<Seat>();
+
+            using (var reader = new StreamReader("input.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    Seat seat = new Seat(line);
+                    seats.Add(seat);
+                }
+            }
+
+            int min = seats.Min(s => s.GetSeatNo());
+            int max = seats.Max(s => s.GetSeatNo());
+            var range = Enumerable.Range(min, max-min);
+            var seatNos = seats.Select(s => s.GetSeatNo()).ToArray();
+
+            var missing = range.Except(seatNos).First();
+            
+            Console.WriteLine($"My seat ID: {missing}");
         }
     }
 }
