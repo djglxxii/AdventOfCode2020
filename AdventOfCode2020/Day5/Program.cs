924e1e648109437d9159b7a00ca4709a99d85e84
David J. Gardner <djglxxii@outlook.com>
2020-12-05 11:46:41 -0500

    Day 5 part 1 solution

---
+++ AdventOfCode2020/Day5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1Solution();
+        }
+
+        private static void Part1Solution()
+        {
+            var seats = new List<Seat>();
+
+            using (var reader = new StreamReader("input.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    Seat seat = new Seat(line);
+                    seats.Add(seat);
+                }
+            }
+
+            var highestSeat = seats.Max(s => s.GetSeatNo());
+
+            Console.WriteLine(highestSeat);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AdventOfCode2020/Day5/Seat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    public class Seat
+    {
+        private readonly string _seatCode;
+
+        public Seat(string seatCode)
+        {
+            _seatCode = seatCode;
+        }
+
+        public int GetSeatNo()
+        {
+            var sb = new StringBuilder(_seatCode);
+            sb.Replace("F", "0");
+            sb.Replace("B", "1");
+            sb.Replace("R", "1");
+            sb.Replace("L", "0");
+
+            return Convert.ToInt32(sb.ToString(), 2);
+        }
+    }
+}
