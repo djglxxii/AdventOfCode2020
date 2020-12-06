0f403c5dbd2190ba1de9ad7c771938c5ae8ca14b
David J. Gardner <djglxxii@outlook.com>
2020-12-06 00:29:05 -0500

    day 6, part 1 solution

---
+++ AdventOfCode2020/Day6/GroupQuestionnaire.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Day6
+{
+    public class GroupQuestionnaire
+    {
+        private readonly HashSet<char> _answers = new HashSet<char>();
+        
+        public void AddResult(string result)
+        {
+            foreach (char c in result)
+            {
+                _answers.Add(c);
+            }
+        }
+
+        public int GetYesCount()
+        {
+            return _answers.Count;
+        }
+    }
+}
---
+++ AdventOfCode2020/Day6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day6
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var questionnaires = new List<GroupQuestionnaire>();
+
+            using (var reader = new StreamReader("input.txt"))
+            {
+                var gq = new GroupQuestionnaire();
+                
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (string.IsNullOrEmpty(line))
+                    {
+                        questionnaires.Add(gq);
+                        gq = new GroupQuestionnaire();
+                    }
+                    gq.AddResult(line);
+                }
+                questionnaires.Add(gq);
+            }
+
+            int sum = questionnaires.Sum(s => s.GetYesCount());
+            Console.WriteLine(sum);
+        }
+    }
+}
